1d6bba438ee2b99421a3f5dcc2f9a7d3a4f10688
x-multibyte <x.multibyte@gmail.com>
2026-02-27 17:37:46 +0800

    feat: add rainbow color effect to ASCII banner

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -13,17 +13,6 @@ use Symfony\Component\Console\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
-    private const BANNER = <<<'BANNER'
- _                                           _         ______                
-(_)                                         | |       (______)               
- _        _____   ____  _____  _   _  _____ | | _____  _     _  _____  _   _ 
-| |      (____ | / ___)(____ || | | || ___ || |(_____)| |   | || ___ || | | |
-| |_____ / ___ || |    / ___ | \ V / | ____|| |       | |__/ / | ____| \ V / 
-|_______)\_____||_|    \_____|  \_/  |_____) \_)      |_____/  |_____)  \_/  
-
-
-BANNER;
-    
     public function __construct()
     {
         parent::__construct('laravel-dev', 'v1.1.0');
@@ -33,7 +22,48 @@ BANNER;
     
     public function getHelp(): string
     {
-        return self::BANNER . "  <info>https://laravel-dev.xmultibyte.com</info>\n\n" . parent::getHelp();
+        $banner = <<<'BANNER'
+ _                                           _         ______                
+(_)                                         | |       (______)               
+ _        _____   ____  _____  _   _  _____ | | _____  _     _  _____  _   _ 
+| |      (____ | / ___)(____ || | | || ___ || |(_____)| |   | || ___ || | | |
+| |_____ / ___ || |    / ___ | \ V / | ____|| |       | |__/ / | ____| \ V / 
+|_______)\_____||_|    \_____|  \_/  |_____) \_)      |_____/  |_____)  \_/  
+BANNER;
+        
+        $coloredBanner = $this->colorizeBanner($banner);
+        
+        return "\n" . $coloredBanner . "\n\n  <info>https://laravel-dev.xmultibyte.com</info>\n\n" . parent::getHelp();
+    }
+    
+    private function colorizeBanner(string $banner): string
+    {
+        $output = '';
+        $charCount = 0;
+        $frequency = 0.3;
+        
+        for ($i = 0; $i < strlen($banner); $i++) {
+            $char = $banner[$i];
+            
+            if ($char === "\n") {
+                $output .= "\n";
+                continue;
+            }
+            
+            if ($char !== ' ') {
+                $charCount++;
+                $red = (int) (sin($frequency * $charCount + 0) * 127 + 128);
+                $green = (int) (sin($frequency * $charCount + 2 * pi() / 3) * 127 + 128);
+                $blue = (int) (sin($frequency * $charCount + 4 * pi() / 3) * 127 + 128);
+                
+                // Use direct ANSI escape codes to avoid Symfony tag parsing issues
+                $output .= sprintf("\033[38;2;%d;%d;%dm%s\033[0m", $red, $green, $blue, $char);
+            } else {
+                $output .= ' ';
+            }
+        }
+        
+        return $output;
     }
     
     private function registerCommands(): void
