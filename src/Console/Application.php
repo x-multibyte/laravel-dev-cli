5de9785fb4f7c10b03e8b807f5984c8af30349d0
x-multibyte <x.multibyte@gmail.com>
2026-02-28 12:12:50 +0800

    feat: add automatic version bump release workflow
    - Auto-detect version bump from commit messages (feat→minor, fix→patch)
    - Update VERSION constant in Application.php
    - Add version field to composer.json
    - Generate changelog from commits

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -13,59 +13,63 @@ use XMultibyte\LaravelDev\Console\Commands\SkillCommand;
 
 class Application extends BaseApplication
 {
+    public const NAME    = 'laravel-dev';
+    public const VERSION = 'v1.2.0';
+    
     public function __construct()
     {
-        parent::__construct('laravel-dev', 'v1.1.1');
-
+        parent::__construct(Application::NAME, Application::VERSION);
+        
         $this->registerCommands();
     }
-
+    
     public function getHelp(): string
     {
         $banner = <<<'BANNER'
- _                                           _         ______                
-(_)                                         | |       (______)               
- _        _____   ____  _____  _   _  _____ | | _____  _     _  _____  _   _ 
+ _                                           _         ______
+(_)                                         | |       (______)
+ _        _____   ____  _____  _   _  _____ | | _____  _     _  _____  _   _
 | |      (____ | / ___)(____ || | | || ___ || |(_____)| |   | || ___ || | | |
-| |_____ / ___ || |    / ___ | \ V / | ____|| |       | |__/ / | ____| \ V / 
-|_______)\_____||_|    \_____|  \_/  |_____) \_)      |_____/  |_____)  \_/  
+| |_____ / ___ || |    / ___ | \ V / | ____|| |       | |__/ / | ____| \ V /
+|_______)\_____||_|    \_____|  \_/  |_____) \_)      |_____/  |_____)  \_/
 BANNER;
-
+        
         $coloredBanner = $this->colorizeBanner($banner);
-
-        return "\n" . $coloredBanner . "\n\n  <info>https://laravel-dev.xmultibyte.com</info>\n\n" . parent::getHelp();
+        
+        return "\n".$coloredBanner."\n\n  <info>https://laravel-dev.xmultibyte.com</info>\n\n".parent::getHelp();
     }
-
-    private function colorizeBanner(string $banner): string
+    
+    private function colorizeBanner( string $banner ): string
     {
-        $output = '';
+        $output    = '';
         $charCount = 0;
         $frequency = 0.3;
-
+        
         for ($i = 0; $i < strlen($banner); $i++) {
             $char = $banner[$i];
-
+            
             if ($char === "\n") {
                 $output .= "\n";
                 continue;
             }
-
+            
             if ($char !== ' ') {
                 $charCount++;
-                $red = (int) (sin($frequency * $charCount + 0) * 127 + 128);
-                $green = (int) (sin($frequency * $charCount + 2 * pi() / 3) * 127 + 128);
-                $blue = (int) (sin($frequency * $charCount + 4 * pi() / 3) * 127 + 128);
-
+                $red   = (int) ( sin($frequency * $charCount + 0) * 127 + 128 );
+                $green = (int) ( sin($frequency * $charCount + 2 * pi() / 3) * 127 + 128 );
+                $blue  = (int) ( sin($frequency * $charCount + 4 * pi() / 3) * 127 + 128 );
+                
                 // Use direct ANSI escape codes to avoid Symfony tag parsing issues
                 $output .= sprintf("\033[38;2;%d;%d;%dm%s\033[0m", $red, $green, $blue, $char);
-            } else {
+            }
+            else {
                 $output .= ' ';
             }
         }
-
+        
         return $output;
     }
-
+    
     private function registerCommands(): void
     {
         $this->addCommand(new SkillCommand);
